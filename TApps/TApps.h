251049d07c80d5fd35dc78fc43de13f9cd95f07b
MuhammadjonTohirov <tohirov.muhammadjon@mail.ru>
2020-03-06 11:14:00 +0500

    Initial Commit

---
+++ TApps/TApps.h
@@ -0,0 +1,19 @@
+//
+//  TApps.h
+//  TApps
+//
+//  Created by INTECH on 3/6/20.
+//  Copyright © 2020 INTECH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for TApps.
+FOUNDATION_EXPORT double TAppsVersionNumber;
+
+//! Project version string for TApps.
+FOUNDATION_EXPORT const unsigned char TAppsVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <TApps/PublicHeader.h>
+
+
