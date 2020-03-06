a484c5f5f1652ba69ae3bf4a31f53f777820f39d
MuhammadjonTohirov <tohirov.muhammadjon@mail.ru>
2020-03-06 17:44:36 +0500

    addsubviews

--- TApps/Templates/Extensions/UIViewController+Extensions.swift
+++ TApps/Templates/Extensions/UIViewController+Extensions.swift
@@ -9,11 +9,11 @@
 import UIKit
 
 extension UIViewController {
-    func addSubview(_ view: UIView) {
+    open func addSubview(_ view: UIView) {
         self.view.addSubview(view)
     }
     
-    func addSubviews(_ views: UIView...) {
+    open func addSubviews(_ views: UIView...) {
         views.forEach { (v) in
             self.view.addSubview(v)
         }
