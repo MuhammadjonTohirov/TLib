4b3b2444d5b3feb75357fee8463d98687fc86991
Mukhammadjon <mukhammadjon@2shop.uz>
2020-09-04 14:34:22 +0500

    bug fix

--- TApps/Templates/Helpers/UI/Model and Listeners/FormInputListener.swift
+++ TApps/Templates/Helpers/UI/Model and Listeners/FormInputListener.swift
@@ -59,7 +59,7 @@ public class FormInputListener: NSObject {
     }
     
     internal func logicForPhoneValue(_ text: String, value: PhoneValue) {
-        let format = value.mask.formprivatisedat
+        let format = value.mask.format
         let formatter = DefaultTextFormatter(textPattern: value.mask.format, patternSymbol: "#")
         
         var phoneText = text.removeSpaces().separateBySpace(format: format)
